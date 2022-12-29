59eecb90af2031a1715697f830c1792210377e01
kangwongu <kwg0527@naver.com>
2022-12-29 23:47:42 +0900

    Feat: Create order entity

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne()
+    @JoinColumn(name = "member_id") // 연관관계 주인
+    private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime orderDate;
+
+    // 주문상태 (Enum)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+}
