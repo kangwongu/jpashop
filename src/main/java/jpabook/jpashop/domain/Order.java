b98b2e1c74fce348ae0acdb808a40b58bba30bd7
kangwongu <kwg0527@naver.com>
2023-01-01 22:14:45 +0900

    Feat: Update order entity 지연로딩 설정

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,11 +17,11 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne()
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id") // 연관관계 주인
     private Member member;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
