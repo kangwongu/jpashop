87bf764d17e4ae51e1f62073a0984e834e50c561
kangwongu <kwg0527@naver.com>
2023-01-03 22:38:02 +0900

    Feat: Update memberrepository

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByName(String name);
 }
