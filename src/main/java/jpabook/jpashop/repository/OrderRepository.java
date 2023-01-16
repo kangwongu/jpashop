683a3bf36d2725273c1f44ac09fc0c0047de6144
kangwongu <kwg0527@naver.com>
2023-01-16 23:53:20 +0900

    Feat: Create order search

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,8 +2,18 @@ package jpabook.jpashop.repository;
 
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    @Query(value = "select o from Order o join o.member m" +
+                    " where o.status = :status " +
+                    " and m.name like :name " +
+                    " limit 1000")
+    List<Order> findAll(OrderSearch orderSearch, @Param("status") OrderStatus orderStatus, @Param("name") String memberName);
 }
